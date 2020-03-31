b7a36214b28fd5f8d5241e18ab85cfb45105ce32
JDaniloC <jdsc@cin.ufpe.br>
2020-03-31 15:12:25 -0300

    Funcionalidades básicas

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -1,42 +1,130 @@
-// GAMBIARRA
-load()
-var utils;
-async function load() {
-    utils = await import('./utils.js');
-}
+import conversorDeTexto from './utils.js';
 
 let reader = new FileReader()
-reader.onloadend = function() {
-    var lista = utils.conversorDeTexto(reader.result)
-    lista.forEach(linha => {
-        adicionaLinha(linha)
-    });
+var listaDePrioridades = []
+var listaDeContextos = []
+var listaDeProjetos = []
+const intensidade = 40
+
+window.dragOver = function dragOver(evt) {
+    evt.preventDefault()
+}
+window.drop = function drop(evt) {
+    /* Para carregar o arquivo
+     */
+    evt.preventDefault()
+    var file = evt.dataTransfer.files[0]
+    
+    if (file == undefined) {
+        var result = conversorDeTexto(evt.dataTransfer.getData('text'))
+
+        result.forEach(item => {
+            adicionaLinhaTarefas(item)
+        });
+    } else {
+        reader.readAsText(file)
+    }
 }
 
-function abrirArquivo(entrada) {
-    var path = entrada.target.files[0]
+function adicionarLinha(id, novo) {
+    /* (4)
+     * Recebe o ID e uma nova informação
+     * e adiciona uma nova Li na Ul da ID
+     */
+    const contexto = document.querySelector(`#${id}`)
+    var novaLinha = document.createElement('li')
 
-    reader.readAsText(path)
+    novaLinha.innerHTML = novo
+
+    contexto.appendChild(novaLinha)
 }
 
-function adicionaLinha(texto) {
-    var lista = document.getElementsByClassName('lista')[0]
+function adicionaLinhaTarefas(texto) {
+    /* (3)
+     * Recebe uma linha do formato:
+     *      (descricao,(data, hora, prioridade, contexto, projeto))
+     * Cria uma nova linha e adiciona cor.
+     */
+    var tarefas = document.getElementsByClassName('tarefas')[0]
     var novaLinha = document.createElement("LI")
 
-    novaLinha.innerHTML = texto
-    switch (texto[0]) {
-        case 'A':
-            novaLinha.style.backgroundColor = '#FF432B'
+    novaLinha.innerHTML = texto[1].slice(0, 3).join(' ') + 
+        " " + texto[0].join(' ') + " " + texto[1].slice(3).join(' ')
+
+    if (texto[1][2] != '' && listaDePrioridades.indexOf(texto[1][2]) == -1) {
+        listaDePrioridades.push(texto[1][2])
+    }
+
+    texto[1].slice(3).forEach(string => {
+        if (string != '') {
+            if (string[0] == "+"
+                && listaDeProjetos.indexOf(string) == -1) {
+                listaDeProjetos.push(string)
+            } else if (string[0] == "@"
+                && listaDeContextos.indexOf(string) == -1) {
+                listaDeContextos.push(string)
+            } 
+        }
+    });
+
+    switch (texto[1][2]) {
+        case '(A)':
+            novaLinha.style.backgroundImage = `linear-gradient(to right, #FF2212 0%, #FFF ${intensidade}%)`
             break
-        case 'B':
-            novaLinha.style.backgroundColor = '#E8BA1C'
+        case '(B)':
+            novaLinha.style.backgroundImage = `linear-gradient(to right, #EBDD19 0%, #FFF ${intensidade}%)`
             break
-        case 'C':
-            novaLinha.style.backgroundColor = '#691CE8'
+        case '(C)':
+            novaLinha.style.backgroundImage = `linear-gradient(to right, #053AE8 0%, #FFF ${intensidade}%)`
             break
-        case 'D':
-            novaLinha.style.backgroundColor = '#43FF40'
+        case '(D)':
+            novaLinha.style.backgroundImage = `linear-gradient(to right, #37FF05 0%, #FFF ${intensidade}%)`
             break
     }
-    lista.appendChild(novaLinha)
+    tarefas.appendChild(novaLinha)
+}
+
+reader.onloadend = function() {
+    /* (2)
+     * Ao terminar a leitura, ele vai converter
+     * o texto, e para cada linha entrará no 
+     * adicionarLinha.
+     */
+    var listaDeTarefas = conversorDeTexto(reader.result)
+
+    var lenPrioridades = listaDePrioridades.length
+    var lenContextos = listaDeContextos.length
+    var lenProjetos = listaDeProjetos.length
+
+    listaDeTarefas.forEach(linha => {
+        if (linha[0] != '') {
+            adicionaLinhaTarefas(linha)
+        }
+    });
+    if (lenPrioridades < listaDePrioridades.length) {
+        listaDePrioridades.forEach(prioridade => {
+            adicionarLinha('prioridades', prioridade)
+        })
+    }
+    if (lenContextos < listaDeContextos.length) {
+        listaDeContextos.forEach(contexto => {
+            adicionarLinha('contextos', contexto)
+        });
+    }
+    if (lenProjetos < listaDeProjetos.length) {
+        listaDeProjetos.forEach(projeto => {
+            adicionarLinha('projetos', projeto)
+        });
+    }
+}
+
+window.abrirArquivo = function abrirArquivo(entrada) {
+    /*
+     * (1) 
+     * Ele recebe o evento, que tem o arquivo dentro, 
+     * e envia para o reader.
+     */
+    var file = entrada.target.files[0]
+    
+    reader.readAsText(file)
 }
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -5,7 +5,11 @@
 (D) #43FF40
 */
 
-export function conversorDeTexto(arquivo) {
+export default function conversorDeTexto(arquivo) {
+    /* (1)
+     * Recebe o texto do arquivo, quebra em linhas
+     * e devolve organizado.
+     */
     var linhas = organizar(arquivo.split("\n"))
 
     return linhas
@@ -13,45 +17,145 @@ export function conversorDeTexto(arquivo) {
 
 function organizar(lista) {
     /*
-    -> Devolve uma lista de listas com as informações das atividades organizadas.
-    lista: Uma lista de strings representando as atividades.
-
-    Transforma as prioridades em maiúsculas e aceita mais de um projeto/descricao.
-    
-    Return: Devolve uma tupla de strings do formato (descricao,(data, hora, prioridade, contexto, projeto...))
+     * -> Devolve uma lista de listas com as informações das atividades organizadas.
+     * lista: Uma lista de strings representando as atividades.
+     *
+     * Transforma as prioridades em maiúsculas e aceita mais de um projeto/descricao.
+     * 
+     * Return: Devolve uma lista de strings do formato
+     *  [descricao,[data, hora, prioridade, contexto, projeto]]
     */
     var resultado = []
     lista.forEach(linhas => {
-        data, hora, pri, desc = '', '', '', ''
+        var data= ''
+        var hora = ''
+        var prioridade = ''
+        var contextoProjeto = []
 
         var task = linhas.split(' ')
+        if (dataValida(task[0])) {
+            data  = task.shift()
+        }
+        if (horaValida(task[0])) {
+            hora = task.shift()
+        }
         if (prioridadeValida(task[0])) {
-            pri = task.shift().toUpperCase()
+            prioridade = task.shift().toUpperCase()
         }
         
+        while (task.length != 0 && (projetoValido(task[task.length - 1]) || contextoValido(task[task.length - 1]))) {
+            contextoProjeto.push(task.pop().trim())
+        }
+
+        resultado = [...resultado, [task, [data, hora, prioridade, ...contextoProjeto]]]
     }); 
     return resultado
 }
 
-function dataValida() {
+function dataValida(string) {
+    /* -> Recebe uma string e verifica se tem exatamente oito caracteres;
+     * Se tao todos digitos e se os dois primeiros correspondem a um dia valido;
+     * Se o terceiro e o quarto correspondem a um mes valido e 
+     * se os quatro ultimos correspondem a um ano valido. 
+     * Checa tambem se o dia e o mes fazem sentido juntos 
+     * (se o dia poderia ocorrer naquele mes).
+     * Alem de verificar se o mes e um numero entre 1 e 12.
+     * Para fevereiro, considera que pode haver ate 29 dias, 
+     * sem se preocupar se o ano e bissexto ou nao. 
+     * Param data = String do formato DDMMAAAA, apenas numeros.
+     * Return: Se todas as verificacoes passarem, devolve True. 
+     * Caso contrario, False.
+     */
+    string = string.trim()
+    if (string.length == 8 && soDigitos(string)) {
+        const dia = parseInt(string.slice(0, 2))
+        const mes = parseInt(string.slice(2, 4))
+        if (mes == 2 && dia < 30) {
+            return true
+        } else {
+            if (mes < 8 && ((mes % 2 != 0 && dia < 32) ||
+                (mes % 2 == 0 && dia < 31)
+                )) {
+                return true
+            } else if (mes < 13 && ((mes % 2 != 0 && dia < 31) || 
+                (mes % 2 == 0 && dia < 32)
+                )) {
+                return true
+            }
+        }
+    }
     return false
 }
-function horaValida() {
+function horaValida(string) {
+    /* -> Recebe uma string e verifica se ela tem exatamente quatro caracteres; 
+     * Se tao todos digitos, se os dois primeiros formam um numero entre 00 e 23;
+     * Se os dois ultimos formam um numero inteiro entre 00 e 59. 
+  
+     * Param horaMin = String do formato HHMM, apenas numeros.
+     * Return: Se tudo isso for verdade, ela devolve True. Caso contrario, False. 
+     */
+    if (string != undefined) {
+        string = string.trim()
+        if (string.length == 4 && soDigitos(string) && 
+            parseInt(string.slice(0, 2)) < 24 
+            && parseInt(string.slice(2)) < 60) {
+            return true
+        }
+    }
     return false
 }
-function prioridadeValida(node) {
+function prioridadeValida(string) {
     /*
-    -> Recebe uma string e verifica se tem exatamente tres caracteres;
-    Se o primeiro e ‘(’, se o terceiro e ‘)’ e se o segundo e uma letra entre A e Z. 
-    Funcionar tanto para letras minusculas quanto maiusculas. 
-    
-    Param pri = String, do formato (S), onde S = A-Z maiusculo ou minusculo.
-    Return: Devolve True se as verificacoes passarem e False caso contrario.
-    */
-    if (node.length == 3 && 
-        ['(A)', '(B)', '(C)', '(D)'].includes(node.toUpperCase())
+     * -> Recebe uma string e verifica se tem exatamente tres caracteres;
+     * Se o primeiro eh ‘(’, se o terceiro eh ‘)’ e se o segundo eh uma letra 
+     * entre A e Z. 
+     * Funcionar tanto para letras minusculas quanto maiusculas. 
+     *
+     * Param string = String, do formato '(S)', onde S = A-Z maiusculo ou minusculo.
+     * Return: Devolve true se as verificacoes passarem e false caso contrario.
+     */
+    if (string != undefined && string.trim().length == 3 &&
+        string[0] + string[2] == '()' && 
+        ((ord('z') >= ord(string[1]) && ord(string[1]) >= ord('a')) || 
+        (ord('A') <= ord(string[1]) && ord(string[1]) <= ord('Z')))
         ) {
         return true
+    }
+    return false
+}
+function contextoValido(string) {
+    /* -> Recebe uma string e verifica se tem pelo menos dois caracteres 
+     * e se o primeiro eh ‘@’. 
+     *
+     * Param string = String, do formato @String.
+     * Return: Devolve True se as verificacoes passarem e False caso contrario.
+     */
+    if (string.length > 1 && string[0] == '@') {
+        return true
+    }
+    return false
+}
+function projetoValido(string) {
+    /* -> Recebe uma string e verifica se tem pelo menos dois caracteres 
+     * e se o primeiro eh ‘+’. 
+     *
+     * Param string = String, do formato +String.
+     * Return: Devolve True se as verificacoes passarem e False caso contrario.
+     */
+    if (string.length > 1 && string[0] == '+') {
+        return true
+    }
+    return false
+}
+
+function ord(str) {
+    return str.charCodeAt(0);
+}
+function soDigitos(str) {
+    for (var i in str) {
+        if (ord(str[i]) <= ord('9') && ord(str[i]) >= ord('0')) {
+            return true
+        }
     }
     return false
 }
